f9cbbaac019f267602aea9e3b1c7d47acd26aa55
OhHaneol <otyvs1109@hs.ac.kr>
2024-05-22 20:47:06 +0900

    style

--- front_hodu/src/main/resources/static/script/kakaoMap.js
+++ front_hodu/src/main/resources/static/script/kakaoMap.js
@@ -1,7 +1,6 @@
-
-        const container = document.getElementById('map');
-        const options = {
-    	    center: new kakao.maps.LatLng(33.421332, 126.6719281),
-    	    level: 3
-        };
-        var map = new kakao.maps.Map(container, options);
+const container = document.getElementById('map');
+const options = {
+    center: new kakao.maps.LatLng(33.421332, 126.6719281),
+    level: 3
+};
+var map = new kakao.maps.Map(container, options);
--- front_hodu/src/main/resources/static/script/subscribeModal.js
+++ front_hodu/src/main/resources/static/script/subscribeModal.js
@@ -1,15 +1,11 @@
-    const modal = document.querySelector('.modal');
-    const modalOpen = document.querySelector('#sub-btn');
-    const modalClose = document.querySelector('.close-btn');
+const modal = document.querySelector('.modal');
+const modalOpen = document.querySelector('#sub-btn');
+const modalClose = document.querySelector('.close-btn');
 
-    <!--    열기 버튼을 누르면 모달팝업이 열림-->
-    modalOpen.addEventListener('click', function(){
-    <!--    'on' class 추가-->
-        modal.classList.add('subOn');
-    });
+modalOpen.addEventListener('click', function(){
+    modal.classList.add('subOn');
+});
 
-    <!--    닫기 버튼을 누르면 모달팝업이 닫힘-->
-    modalClose.addEventListener('click', function(){
-    <!--        'on' class 제거-->
-        modal.classList.remove('subOn');
-    });
+modalClose.addEventListener('click', function(){
+    modal.classList.remove('subOn');
+});
