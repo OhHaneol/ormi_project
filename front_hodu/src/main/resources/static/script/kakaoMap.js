df63fc983f3a0cfc910cb1bff4fb916e4312bea3
OhHaneol <otyvs1109@hs.ac.kr>
2024-05-28 01:08:43 +0900

    feat: change map api

--- front_hodu/src/main/resources/static/script/kakaoMap.js
+++ front_hodu/src/main/resources/static/script/kakaoMap.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('map');
 const options = {
-    center: new kakao.maps.LatLng(33.421332, 126.6719281),
+    center: new kakao.maps.LatLng(33.442347233910816, 126.57191223611196),
     level: 3
 };
 var map = new kakao.maps.Map(container, options);
