52116942a4d41d7f89790789068e544dc19f7076
OhHaneol <otyvs1109@hs.ac.kr>
2024-05-28 01:37:20 +0900

    feat: add map control

--- front_hodu/src/main/resources/static/script/kakaoMap.js
+++ front_hodu/src/main/resources/static/script/kakaoMap.js
@@ -1,6 +1,24 @@
 const container = document.getElementById('map');
 const options = {
-    center: new kakao.maps.LatLng(33.442347233910816, 126.57191223611196),
+    center: new kakao.maps.LatLng(33.4423472, 126.5719122), // 지도 중심좌표
     level: 3
 };
-var map = new kakao.maps.Map(container, options);
+let map = new kakao.maps.Map(container, options);           // 지도 생성
+
+// 일반-스카이뷰 전환 컨트롤 생성 및 추가
+let mapTypeControl = new kakao.maps.MapTypeControl();
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// 확대 축소 컨트롤 생성 및 추가
+let zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+// 마커
+let markerPosition  = new kakao.maps.LatLng(33.4423472, 126.5719122);
+let marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+marker.setMap(map);
+
+// 축적
+map.setCopyrightPosition(kakao.maps.CopyrightPosition.BOTTOMRIGHT, true);
